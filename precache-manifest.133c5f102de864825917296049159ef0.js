b075ed3e423c77683d8112ce59245dd15547c768
Pratyusha Chakraborty <pratyushachakraborty2001@gmail.com>
2023-11-28 23:14:16 +0530

    Updates

---
+++ precache-manifest.133c5f102de864825917296049159ef0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1be91512ee3457f3612",
+    "url": "/static/css/main.f51d9561.chunk.css"
+  },
+  {
+    "revision": "e1be91512ee3457f3612",
+    "url": "/static/js/main.e1be9151.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7b2f8304b0222f97ca6d19a83fa841dd",
+    "url": "/index.html"
+  }
+];
